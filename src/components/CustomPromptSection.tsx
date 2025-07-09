3e98b5f84db1daad4d713e9e344347fc53beccf1
Lâm Minh Đăng <2603lamminhdang@gmail.com>
2025-07-09 10:03:49 +0700

    small update

--- src/components/CustomPromptSection.tsx
+++ src/components/CustomPromptSection.tsx
@@ -169,7 +169,7 @@ const CustomPromptSection: React.FC = () => {
           padding: "2rem 1.5rem 1.5rem 1.5rem",
           display: "flex",
           flexDirection: "column",
-          justifyContent: "flex-start",
+          justifyContent: "space-between",
         }}
       >
         <h2 style={{ marginTop: 0, color: "#38bdf8" }}>
